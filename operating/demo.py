d35eea390a24e7addd381ec80da47c816fce03fc
pinjesz_komputer <jasiekjeschke@gmail.com>
2023-01-13 23:50:49 +0100

    dgdgd

--- operating/demo.py
+++ operating/demo.py
@@ -1,9 +1,9 @@
 import RPi.GPIO as GPIO
 import time
 from pynput.keyboard import Key, Listener
+import keyboard
 
-
-#pins:
+# pins:
 OUTER_STEP = 13
 OUTER_DIR = 15
 OUTER_M0 = 7
@@ -12,6 +12,7 @@ OUTER_M1 = 11
 INNER_STEP = 16
 INNER_DIR = 12
 INNER_M0 = 18
+
 
 def setup_pins():
     GPIO.setmode(GPIO.BOARD)
@@ -31,35 +32,19 @@ def setup_pins():
 def on_press(key):
     info = False
     if key == Key.left:
-        GPIO.output(OUTER_DIR, GPIO.LOW)
-        GPIO.output(OUTER_STEP, GPIO.HIGH)
-        time.sleep(0.01)
-        GPIO.output(OUTER_DIR, GPIO.LOW)
-        time.sleep(0.01)
+        left()
         info = True
 
     if key == Key.right:
-        GPIO.output(OUTER_DIR, GPIO.HIGH)
-        GPIO.output(OUTER_STEP, GPIO.HIGH)
-        time.sleep(0.01)
-        GPIO.output(OUTER_DIR, GPIO.LOW)
-        time.sleep(0.01)
+        right()
         info = True
 
     if key == Key.up:
-        GPIO.output(INNER_DIR, GPIO.HIGH)
-        GPIO.output(INNER_STEP, GPIO.HIGH)
-        time.sleep(0.01)
-        GPIO.output(INNER_STEP, GPIO.LOW)
-        time.sleep(0.01)
+        up()
         info = True
 
     if key == Key.down:
-        GPIO.output(INNER_DIR, GPIO.LOW)
-        GPIO.output(INNER_STEP, GPIO.HIGH)
-        time.sleep(0.01)
-        GPIO.output(INNER_STEP, GPIO.LOW)
-        time.sleep(0.01)
+        down()
         info = True
 
     if info:
@@ -70,12 +55,45 @@ def on_release(key):
     if key == Key.esc:
         return False
 
+
+def left():
+    GPIO.output(OUTER_DIR, GPIO.LOW)
+    GPIO.output(OUTER_STEP, GPIO.HIGH)
+    time.sleep(0.01)
+    GPIO.output(OUTER_DIR, GPIO.LOW)
+    time.sleep(0.01)
+
+
+def right():
+    GPIO.output(OUTER_DIR, GPIO.HIGH)
+    GPIO.output(OUTER_STEP, GPIO.HIGH)
+    time.sleep(0.01)
+    GPIO.output(OUTER_DIR, GPIO.LOW)
+    time.sleep(0.01)
+
+
+def up():
+    GPIO.output(INNER_DIR, GPIO.HIGH)
+    GPIO.output(INNER_STEP, GPIO.HIGH)
+    time.sleep(0.01)
+    GPIO.output(INNER_STEP, GPIO.LOW)
+    time.sleep(0.01)
+
+
+def down():
+    GPIO.output(INNER_DIR, GPIO.LOW)
+    GPIO.output(INNER_STEP, GPIO.HIGH)
+    time.sleep(0.01)
+    GPIO.output(INNER_STEP, GPIO.LOW)
+    time.sleep(0.01)
+
+
 def main():
-    # Collect events until released
-    with Listener(
-            on_press=on_press,
-            on_release=on_release) as listener:
-        listener.join()
+    keyboard.on_press_key("left", left)
+    keyboard.on_press_key("right", right)
+    keyboard.on_press_key("down", down)
+    keyboard.on_press_key("up", up)
+
 
 if __name__ == "__main__":
     main()
