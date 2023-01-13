564dbea2528aa3d7057180fe4e983d4cbc2cf9fe
pinjesz_komputer <jasiekjeschke@gmail.com>
2023-01-13 21:28:49 +0100

    third

--- pin.py
+++ pin.py
@@ -6,23 +6,26 @@ import time
 
 STEP_z = 16
 DIR_z = 12
+M0_z = 18
 
 GPIO.setmode(GPIO.BOARD)
 
 GPIO.setup(STEP_z, GPIO.OUT)
 GPIO.setup(DIR_z, GPIO.OUT)
+GPIO.setup(M0_z, GPIO.OUT)
+GPIO.output(M0_z, GPIO.LOW)
 
 while True:
     GPIO.output(DIR_z, GPIO.HIGH)
-    for i in range(20):
-        time.sleep(0.1)
+    for i in range(50):
+        time.sleep(0.01)
         GPIO.output(STEP_z, GPIO.HIGH)
-        time.sleep(0.1)
+        time.sleep(0.01)
         GPIO.output(STEP_z, GPIO.LOW)
 
     GPIO.output(DIR_z, GPIO.LOW)
-    for i in range(20):
-        time.sleep(0.1)
+    for i in range(50):
+        time.sleep(0.01)
         GPIO.output(STEP_z, GPIO.HIGH)
-        time.sleep(0.1)
+        time.sleep(0.01)
         GPIO.output(STEP_z, GPIO.LOW)
