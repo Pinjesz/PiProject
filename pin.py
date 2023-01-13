9e46e2067a5c23474c9f0161dcd13713bbaba1f1
pinjesz_komputer <jasiekjeschke@gmail.com>
2023-01-13 21:14:51 +0100

    first try

---
+++ pin.py
@@ -0,0 +1,25 @@
+import RPi.GPIO as GPIO
+import time
+
+STEP_z = 13
+DIR_z = 15
+
+GPIO.setmode(GPIO.BOARD)
+
+GPIO.setup(STEP_z, GPIO.OUT)
+GPIO.setup(DIR_z, GPIO.OUT)
+
+while True:
+    GPIO.output(DIR_z, GPIO.HIGH)
+    for i in range(20):
+        time.sleep(0.1)
+        GPIO.output(STEP_z, GPIO.HIGH)
+        time.sleep(0.1)
+        GPIO.output(STEP_z, GPIO.LOW)
+
+    GPIO.output(DIR_z, GPIO.LOW)
+    for i in range(20):
+        time.sleep(0.1)
+        GPIO.output(STEP_z, GPIO.HIGH)
+        time.sleep(0.1)
+        GPIO.output(STEP_z, GPIO.LOW)
