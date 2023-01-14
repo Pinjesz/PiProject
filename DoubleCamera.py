6090ccd85da4358e523283fbe5012d26a4d41e03
pinjesz_komputer <jasiekjeschke@gmail.com>
2023-01-14 11:52:24 +0100

    upp

--- DoubleCamera.py
+++ DoubleCamera.py
@@ -1,6 +1,4 @@
-import cv2
 import MPcomms.RTMPVideo.streaming.VideoStreamer as VS
-from MPcomms.RTMPVideo.DummyCap import DummyCap as DCap
 from picamera2 import Picamera2
 
 if __name__ == "__main__":
@@ -8,7 +6,8 @@ if __name__ == "__main__":
     # resolution = (4056, 3040)
 
     cam = Picamera2()
-    preview_config = cam.create_preview_configuration({"format": "BGR888", "size": resolution})
+    preview_config = cam.create_preview_configuration(
+        {"format": "BGR888", "size": resolution})
     cam.configure(preview_config)
     cam.start()
 
@@ -16,15 +15,9 @@ if __name__ == "__main__":
 
     streamer_rgb.run()
 
-    while True:
-        img_rgb = cam.capture_buffer()
-
-        # img_gs = cv2.cvtColor(img_rgb, cv2.COLOR_BGR2GRAY)
-
-        streamer_rgb.publishFrame(img_rgb)
-
-        # cv2.imshow('streamer', img_rgb)
-        # if cv2.waitKey(1) & 0xFF == ord('q'):
-        #     break
-
-    streamer_rgb.close()
+    try:
+        while True:
+            img_rgb = cam.capture_buffer()
+            streamer_rgb.publishFrame(img_rgb)
+    except:
+        streamer_rgb.close()
