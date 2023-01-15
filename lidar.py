cda41e90e55f33c58464c913ee78e81a49c7c4c9
pinjesz_komputer <jasiekjeschke@gmail.com>
2023-01-15 15:09:11 +0100

    fdfds

--- lidar.py
+++ lidar.py
@@ -1,41 +1,27 @@
+# -*- coding: utf-8 -*
 import serial
-import time
-import numpy as np
 
-ser = serial.Serial(
-    port="/dev/serial0",
-    baudrate=115200,
-    parity=serial.PARITY_NONE,
-    stopbits=serial.STOPBITS_ONE,
-    bytesize=serial.EIGHTBITS,
-    timeout=1)
+ser = serial.Serial("/dev/ttyAMA0", 115200)
 
-
-def read():
+def getTFminiData():
     while True:
-        counter = ser.in_waiting  # count the number of bytes of the serial port
-        print(counter)
-        if counter > 8:
-            bytes_serial = ser.read(9)  # read 9 bytes
-            ser.reset_input_buffer()  # reset buffer
-
-            if bytes_serial[0] == 0x59 and bytes_serial[1] == 0x59:  # check first two bytes
-                # distance in next two bytes
-                distance = bytes_serial[2] + bytes_serial[3]*256
-                # signal strength in next two bytes
-                strength = bytes_serial[4] + bytes_serial[5]*256
-                # temp in next two bytes
-                temperature = bytes_serial[6] + bytes_serial[7]*256
-                temperature = (temperature/8.0) - \
-                    256.0  # temp scaling and offset
-                return distance/100.0, strength, temperature
+        count = ser.in_waiting
+        if count > 8:
+            recv = ser.read(9)
+            ser.reset_input_buffer()
+            if recv[0] == 'Y' and recv[1] == 'Y': # 0x59 is 'Y'
+                low = int(recv[2].encode('hex'), 16)
+                high = int(recv[3].encode('hex'), 16)
+                distance = low + high * 256
+                print(distance)
 
 
-if ser.isOpen() == False:
-    ser.open()  # open serial port if not open
-    print("serial opened")
 
-distance, strength, temperature = read()  # read values
-print('Distance: {0:2.2f} m, Strength: {1:2.0f} / 65535 (16-bit), Chip Temperature: {2:2.1f} C'.
-      format(distance, strength, temperature))  # print sample data
-ser.close()  # close serial port
+if __name__ == '__main__':
+    try:
+        if ser.is_open == False:
+            ser.open()
+        getTFminiData()
+    except KeyboardInterrupt:   # Ctrl+C
+        if ser != None:
+            ser.close()
