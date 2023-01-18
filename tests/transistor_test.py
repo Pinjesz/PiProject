800e629551e2460c05c9cdef1fc354f1fc3c75d2
pinjesz_komputer <jasiekjeschke@gmail.com>
2023-01-18 22:16:37 +0100

    fghgryhgf

---
+++ tests/transistor_test.py
@@ -0,0 +1,22 @@
+import RPi.GPIO as GPIO
+import time
+
+
+def main():
+    GPIO.setmode(GPIO.BOARD)
+
+    GPIO.setup(19, GPIO.OUT)
+    GPIO.setup(8, GPIO.IN)
+
+    while True:
+        GPIO.output(19, GPIO.LOW)
+        time.sleep(1)
+        print("low:", GPIO.input(8))
+        time.sleep(1)
+        GPIO.output(19, GPIO.HIGH)
+        time.sleep(1)
+        print("high:", GPIO.input(8))
+        time.sleep(1)
+
+if __name__ == "__main__":
+    main
