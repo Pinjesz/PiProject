e4b1428f00fd40613872da2ce50d38383e5c38a1
pinjesz_komputer <jasiekjeschke@gmail.com>
2023-01-25 21:11:48 +0100

    naming

--- MPcomms/steering.py
+++ MPcomms/steering.py
@@ -3,14 +3,14 @@ import readchar
 import time
 
 # pins:
-OUTER_STEP = 13
-OUTER_DIR = 15
-OUTER_M0 = 7
-OUTER_M1 = 11
+PAN_STEP = 13
+PAN_DIR = 15
+PAN_M0 = 7
+PAN_SWITCH = 11
 
-INNER_STEP = 16
-INNER_DIR = 12
-INNER_M0 = 18
+TILT_STEP = 16
+TILT_DIR = 12
+TILT_SWITCH = 18
 
 LASER = 19
 
@@ -19,46 +19,46 @@ def setup_pins():
     GPIO.setwarnings(False)
     GPIO.setmode(GPIO.BOARD)
 
-    GPIO.setup(OUTER_STEP, GPIO.OUT)
-    GPIO.setup(OUTER_DIR, GPIO.OUT)
-    GPIO.setup(OUTER_M0, GPIO.OUT)
-    GPIO.setup(OUTER_M1, GPIO.OUT)
-    GPIO.setup(INNER_STEP, GPIO.OUT)
-    GPIO.setup(INNER_DIR, GPIO.OUT)
-    GPIO.setup(INNER_M0, GPIO.OUT)
+    GPIO.setup(PAN_STEP, GPIO.OUT)
+    GPIO.setup(PAN_DIR, GPIO.OUT)
+    GPIO.setup(PAN_M0, GPIO.OUT)
+    GPIO.setup(PAN_SWITCH, GPIO.OUT)
+    GPIO.setup(TILT_STEP, GPIO.OUT)
+    GPIO.setup(TILT_DIR, GPIO.OUT)
+    GPIO.setup(TILT_SWITCH, GPIO.OUT)
     GPIO.setup(LASER, GPIO.OUT)
-    GPIO.output(OUTER_M0, GPIO.LOW)
-    GPIO.output(OUTER_M1, GPIO.LOW)
-    GPIO.output(INNER_M0, GPIO.LOW)
+    GPIO.output(PAN_M0, GPIO.LOW)
+    GPIO.output(PAN_SWITCH, GPIO.LOW)
+    GPIO.output(TILT_SWITCH, GPIO.LOW)
     GPIO.output(LASER, GPIO.LOW)
 
 
 def left():
-    GPIO.output(OUTER_DIR, GPIO.HIGH)
-    GPIO.output(OUTER_STEP, GPIO.HIGH)
+    GPIO.output(PAN_DIR, GPIO.HIGH)
+    GPIO.output(PAN_STEP, GPIO.HIGH)
     time.sleep(0.001)
-    GPIO.output(OUTER_STEP, GPIO.LOW)
+    GPIO.output(PAN_STEP, GPIO.LOW)
     time.sleep(0.001)
 
 
 def right():
-    GPIO.output(OUTER_DIR, GPIO.LOW)
-    GPIO.output(OUTER_STEP, GPIO.HIGH)
+    GPIO.output(PAN_DIR, GPIO.LOW)
+    GPIO.output(PAN_STEP, GPIO.HIGH)
     time.sleep(0.001)
-    GPIO.output(OUTER_STEP, GPIO.LOW)
+    GPIO.output(PAN_STEP, GPIO.LOW)
     time.sleep(0.001)
 
 
 def up():
-    GPIO.output(INNER_DIR, GPIO.LOW)
-    GPIO.output(INNER_STEP, GPIO.HIGH)
-    GPIO.output(INNER_STEP, GPIO.LOW)
+    GPIO.output(TILT_DIR, GPIO.LOW)
+    GPIO.output(TILT_STEP, GPIO.HIGH)
+    GPIO.output(TILT_STEP, GPIO.LOW)
 
 
 def down():
-    GPIO.output(INNER_DIR, GPIO.HIGH)
-    GPIO.output(INNER_STEP, GPIO.HIGH)
-    GPIO.output(INNER_STEP, GPIO.LOW)
+    GPIO.output(TILT_DIR, GPIO.HIGH)
+    GPIO.output(TILT_STEP, GPIO.HIGH)
+    GPIO.output(TILT_STEP, GPIO.LOW)
 
 
 def laser_on():
